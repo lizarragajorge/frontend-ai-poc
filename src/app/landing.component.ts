af275e3cf2b1c233ece41792eeecbec418676bbf
Jorge Lizarraga <lizarraga-jorge@live.com>
2025-09-10 22:02:27 -0500

    address PR suggestion for router

--- src/app/landing.component.ts
+++ src/app/landing.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-landing',
 	standalone: true,
+	imports: [RouterLink],
 	template: `
 	<div class="home-shell">
 		<div class="hero hero-card">
